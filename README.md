7355f16a2c6e5efb889ad06c6c5d88a6812f9916
sistemas <Luzaraque3>
2023-03-22 09:14:46 -0500

    Se agrego diagrama, programa y README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # JUEGO_PIEDRA-PAPEL-Y-TIJERA
 Programa para simular JUEGO_PIEDRA,PAPEL Y TIJERA
 
+# Diagrama de flujo
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
--- piedra,papel,tijera.py
+++ piedra,papel,tijera.py
@@ -1,1 +1,41 @@
-#
+#Programa para simular JUEGO_PIEDRA,PAPEL Y TIJERA
+
+import random
+
+print("Piedra: 1")
+print("Papel: 2")
+print("Tijera: 3")
+
+# input
+
+# processing
+P = int(input("Piedra, papel o tijeras: "))
+M = random.randint(1,3)
+
+
+if (M==1):
+    if (P==1):
+        rta= "Empate"
+    elif (P==2):
+        rta= "Perdistes"
+    elif (P==3):
+        rta="Ganaste"
+elif (M==2):
+    if(P==1):
+        rta="Perdistes"
+    elif(P==2):
+        rta="empate"
+    elif(P==3):
+        rta="Ganaste"
+elif(M==3):
+    if(P==1):
+        rta= "Ganaste"
+    elif (P==2):
+        rta="Perdistes"
+    elif (P==3):
+        rta="Empate"
+
+if P>3:
+    print("Lo siento")
+else: 
+    print(rta)
